257a6a6a33d2ff6758763ef372b031c4ef3b30b5
reunion <unionre7@gmail.com>
2019-06-09 17:15:14 -0400

    ReUnion

---
+++ ReUnion/Assets/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public int lives = 3;
+
+    void FixedUpdate()
+    {
+
+
+        if (rb.position.y < -7.5f)
+        {
+            FindObjectOfType<GameSet>().Respawn();
+            lives -= 1; 
+        }
+        if (lives <= 0)
+        {
+            FindObjectOfType<GameSet>().EndGame();
+        }
+        
+    }
+
+}
---
+++ ReUnion/Assets/Stripts/GameSet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSet : MonoBehaviour {
+
+    bool endGame = false;
+    public float RestartTime = 1f;
+
+    public void EndGame()
+    {
+        if (endGame == false)
+        {
+            endGame = true;
+            Invoke("Restart", RestartTime);
+        }
+    }
+    public void Respawn()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    void Restart()
+    {
+        SceneManager.LoadScene("Menu Muerte");
+    }
+
+
+}
