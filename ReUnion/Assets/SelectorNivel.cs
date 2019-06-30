ba108b70b664beaea6088cc9bbc5ae09c6d1b1a1
reunion <unionre7@gmail.com>
2019-06-30 18:24:18 -0400

    Update Menus 3

--- ReUnion/Assets/SelectorNivel.cs
+++ ReUnion/Assets/SelectorNivel.cs
@@ -3,8 +3,14 @@
 public class SelectorNivel : MonoBehaviour
 {
 	public Fader fader;
+
 	public void Seleccionar(string nombre)
 	{
 		fader.FadeTo(nombre);
+	}
+
+	public void VolverMenuP()
+	{
+		fader.FadeTo("Menu");
 	}
 }
