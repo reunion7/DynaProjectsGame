df99bc6ce430d77fda81a671f29f7b765446bac9
reunion <unionre7@gmail.com>
2019-07-02 14:51:04 -0400

    Update

--- ReUnion/Assets/SelectorNivel.cs
+++ ReUnion/Assets/SelectorNivel.cs
@@ -1,9 +1,24 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class SelectorNivel : MonoBehaviour
 {
 	public Fader fader;
+	public Button [] botonesNiveles;
 
+	void Start()
+	{
+		int nivelAlcanzado = PlayerPrefs.GetInt("nivelAlcanzado",1); //falta configurar cuando se completa el nivel, script por crear
+
+
+		for (int i = 0; i < botonesNiveles.Length; i++)
+		{
+			if(i + 1 > nivelAlcanzado)
+			{
+	 			botonesNiveles[i].interactable = false;
+			}
+		}
+	}
 	public void Seleccionar(string nombre)
 	{
 		fader.FadeTo(nombre);
