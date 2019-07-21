84c6cbdc9fe33b253769ad35bfcfdbd64e404a1e
skrlet13 <44346895+skrlet13@users.noreply.github.com>
2019-07-21 17:19:26 -0400

    cambios

---
+++ ReUnion/Assets/Scripts/SFXVol.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class SFXVol : MonoBehaviour {
+
+    public AudioMixer mixer;
+
+    public void SetLevel (float sliderValue)
+    {
+        mixer.SetFloat("SFXVol", Mathf.Log10(sliderValue) * 20);
+    }
+}
