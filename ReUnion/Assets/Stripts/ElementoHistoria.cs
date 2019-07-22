6698706824eed3ae490c6c45194600bb3c27ea62
Fabri211 <52009330+Fabri211@users.noreply.github.com>
2019-07-21 21:20:06 -0400

    Scripts de dialogo
    Tiene scripts de dialogo y un dialogo de prueba.

---
+++ ReUnion/Assets/Stripts/ElementoHistoria.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElementoHistoria : MonoBehaviour
+{
+    public bool playerEnRango;
+    public Dialogo dialogo;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && playerEnRango)
+        {
+            TriggerDialogo();
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D colision)
+    {
+        if (colision.CompareTag("Player"))
+        {
+            playerEnRango = true;
+        }
+
+    }
+    private void OnTriggerExit2D(Collider2D colision)
+    {
+        if (colision.CompareTag("Player"))
+        {
+            playerEnRango = false;
+        }
+
+    }
+
+    public void TriggerDialogo()
+    {
+
+        FindObjectOfType<GestorDialogo>().EmpezarDialogo(dialogo);
+
+    }
+
+}
