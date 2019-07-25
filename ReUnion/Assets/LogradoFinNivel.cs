525bc0f4d0d5afb61c6a992c33fdd301ec68b0e2
Fabri211 <52009330+Fabri211@users.noreply.github.com>
2019-07-25 17:47:41 -0400

    cambios

---
+++ ReUnion/Assets/LogradoFinNivel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogradoFinNivel : MonoBehaviour
+{
+	public string SiguienteNivel;
+	public int NumeroSiguienteNivel;
+	Fader fader;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+		fader = FindObjectOfType<Fader>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		PlayerPrefs.SetInt("nivelAlcanzado", NumeroSiguienteNivel);
+		fader.FadeTo(SiguienteNivel);
+		
+	}
+
+}
